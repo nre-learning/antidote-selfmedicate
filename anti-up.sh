e44787860639927483274f1359b8d16db36610d8
Matt Oswalt <matt@keepingitclassless.net>
2019-02-24 19:28:51 -0800

    linux experiment
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- anti-up.sh
+++ anti-up.sh
@@ -19,26 +19,36 @@ else
 fi
 
 echo "Creating minikube cluster. This can take a few minutes, please be patient..."
-minikube stop > /dev/null
-minikube delete > /dev/null
-minikube start --cpus 4 --memory 8192 --network-plugin=cni --extra-config=kubelet.network-plugin=cni > /dev/null
+sudo minikube stop # > /dev/null
+sudo minikube delete # > /dev/null
+sudo rm -rf /var/lib/minikube/certs/
+sudo rm -rf /etc/kubernetes/
+sudo minikube start --vm-driver=none --cpus 4 --memory 8192 --network-plugin=cni --extra-config=kubelet.network-plugin=cni # > /dev/null
 
 echo "Uploading multus configuration..."
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(minikube ssh-key) multus-cni.conf docker@$(minikube ip):/home/docker/multus.conf  > /dev/null
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(minikube ssh-key) -t docker@$(minikube ip) "sudo cp /home/docker/multus.conf /etc/cni/net.d/1-multus.conf"  > /dev/null
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(minikube ssh-key) -t docker@$(minikube ip) "sudo systemctl restart localkube"  > /dev/null
+#scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(minikube ssh-key) multus-cni.conf docker@$(minikube ip):/home/docker/multus.conf  > /dev/null
+#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(minikube ssh-key) -t docker@$(minikube ip) "sudo cp /home/docker/multus.conf /etc/cni/net.d/1-multus.conf"  > /dev/null
+#ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $(minikube ssh-key) -t docker@$(minikube ip) "sudo systemctl restart localkube"  > /dev/null
+
+sudo cp multus-cni.conf /etc/cni/net.d/1-multus.conf
+sudo systemctl restart kubelet
 
 echo "About to modify /etc/hosts to add record for 'antidote-local'. You will now be prompted for your sudo password."
-sudo sed -i '' '/antidote-local.*/d' /etc/hosts  > /dev/null
-echo "$(minikube ip)    antidote-local" | sudo tee -a /etc/hosts  > /dev/null
+sudo sed -i '' '/antidote-local.*/d' /etc/hosts # > /dev/null
+echo "$(minikube ip)    antidote-local" | sudo tee -a /etc/hosts # > /dev/null
 
 echo "Uploading platform manifests..."
-kubectl create -f weaveinstall.yml > /dev/null
-kubectl create -f multusinstall.yml > /dev/null
-kubectl create -f nginx-controller.yaml > /dev/null
-kubectl create -f syringe.yml > /dev/null
-kubectl create -f antidote-web.yaml > /dev/null
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl create -f multusinstall.yml #> /dev/null
+kubectl create -f nginx-controller.yaml #> /dev/null
+kubectl create -f syringe.yml #> /dev/null
+kubectl create -f antidote-web.yaml #> /dev/null
 
 echo "${GREEN}Finished!${NC} Antidote is being spun up right now. Soon, it will be available at:
 
 https://antidote-local:30002/"
+
+
+# For linux, running without a VM, make sure user is in docker group, or otherwise doesn't need to run sudo for docker
+# run minikube as sudo with linux
+ 
